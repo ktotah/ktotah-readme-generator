928a81d13d2ba728455312948da6676706259f72
ktotah <ket2137@columbia.edu>
2024-02-19 16:29:17 -0500

    flushed out all of the necessary questions in the index.js file in order to meet the acceptance criteria for the challenge. flushed out the entire generateMarkdown.js file with the functions to renderLicenseBadge, renderLicenseLink, renderLicenseSection, and generateMarkdown, making sure to have the markdown file contain all of the necessary sections to meet the acceptance criteria for the challenge.

--- index.js
+++ index.js
@@ -9,22 +9,66 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your project?',
+        message: 'What is the title of your project?'
     }, 
     // question for DESCRIPTION
     {
         type: 'input',
         name: 'description',
-        message: 'Provide a short description of your project:',
+        message: 'Provide a short description of your project:'
     },
-    // Add more questions here
+    // question for INSTALLATION
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    // question for USAGE
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions for use:'
+    },
+    // question for CONTRIBUTING
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?'
+    },
+    // question for TESTS
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can users test your project?'
+    },
+    // question for LICENSE
+    {
+        type: 'list', 
+        name: 'license',
+        message: 'Which license does your project use?', 
+        choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None']
+    },
+    // question for GitHub Username (for QUESTIONS section)
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:'
+    },
+    // questoin for email (for QUESTIONS section)
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
-        if (err) throw err; 
-        console.log('README.md has been generated!');
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your README file has been generated!")
     });
 }
 
@@ -36,7 +80,7 @@ function init() {
         writeToFile('README.md', readmeContent);
     })
     .catch((error) => {
-        if (err.isTtyError) {
+        if (error.isTtyError) {
             console.error(`Prompt couldn't be rendered in the current environment`);
         } else {
             console.error('An error occured:', error);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license){
+  const badgeLinks = {
+    'MIT': 'https://img.shields.io/badge/License-MIT-blue.svg',
+    'Apache 2.0': 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+    'GPLv3': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+    'BSD 3-Clause': 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg'
+  };
+  return license !== 'None' ? `![License](${badgeLinks[license]})` : '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'GPLv3': 'https://www.gnu.org/licenses/gpl-3.0',
+    'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause'
+  };
+  return license !== 'None' ? `* [License](#license)` : '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseText = {
+    'MIT': 'This project is licensed under the MIT License.',
+    'Apache 2.0': 'This project is licensed under the Apache 2.0 license.',
+    'GPLv3': 'This project is licensed under the GNU GPLv3 license.',
+    'BSD 3-Clause': 'This project is licensed under the BSD 3-Clause license.'
+  };
+  return license !== 'None' ? `## Licnese\n\n${licenseText[license]}\n` : '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  ${renderLicenseLink(data.license)}
+  - [Questions](#questions)
+
+  ## Installation 
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+ ${renderLicenseSection(data.license)}
+
+ ## Questions
+ For any questions, please contact me with the information below.
+ GitHub: [${data.github}](https://github.com/${data.github})
+ Email: [${data.email}](mailto:${data.email})`;
 }
 
 module.exports = generateMarkdown;
