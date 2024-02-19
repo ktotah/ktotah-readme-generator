e0578b7668785b80190b859f5f3d7040f0257f95
ktotah <ket2137@columbia.edu>
2024-02-19 15:19:49 -0500

    installed inquirer, which created a package-lock.json file in my repo and updated my package.json file. wrote my first round of code in index.js, where i included the packages needed for this application, began to create an array of questions for user input, created a function to write the README file, and created a function to initialize the app.

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require(`./utils/generateMarkdown`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // question for TITLE
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    }, 
+    // question for DESCRIPTION
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+    },
+    // Add more questions here
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err; 
+        console.log('README.md has been generated!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+        if (err.isTtyError) {
+            console.error(`Prompt couldn't be rendered in the current environment`);
+        } else {
+            console.error('An error occured:', error);
+        }
+    });
+}
 
 // Function call to initialize app
 init();
